bd483df53e0f95837f730e4e67694e5afcea4ec5
Todd Heckeler <todd@consumedesign.com>
2023-10-13 11:24:54 -0400

    redo

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,10 @@
 import Navbar from 'components/layout/header';
-import { ensureStartsWith } from 'lib/utils';
 import { Suspense } from 'react';
 import './globals.css';
 
-const { TWITTER_CREATOR, TWITTER_SITE, SITE_NAME } = process.env;
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : 'http://localhost:3000';
-const twitterCreator = TWITTER_CREATOR ? ensureStartsWith(TWITTER_CREATOR, '@') : undefined;
-const twitterSite = TWITTER_SITE ? ensureStartsWith(TWITTER_SITE, 'https://') : undefined;
 
 export const metadata = {
   metadataBase: new URL(baseUrl),
@@ -19,15 +15,7 @@ export const metadata = {
   robots: {
     follow: true,
     index: true
-  },
-  ...(twitterCreator &&
-    twitterSite && {
-      twitter: {
-        card: 'summary_large_image',
-        creator: twitterCreator,
-        site: twitterSite
-      }
-    })
+  }
 };
 
 export default function RootLayout({ children }) {
